17830849e641fbbacdc1aedf114a50dafa061af4
maxbrzhnk <makasa4d@gmail.com>
2022-05-17 08:47:05 +0300

    Add sample test

---
+++ ConsoleTest/MainTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleTest;
+
+[TestClass]
+public class MainTest
+{
+    [TestMethod]
+    public void SampleTest()
+    {
+        Assert.IsTrue(true);
+    }
+}
